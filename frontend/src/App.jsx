39bd7d03438a161befbd242808a34079cbf0bbcb
Sameer-kulkarni-123 <sameerk2205@gmail.com>
2025-07-10 19:29:08 +0530

    add frontend5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 // src/App.js
 import React, { useState } from "react";
 import MetaMaskAuth from "./components/MetaMaskAuth";
+import RegisterCrateForm from "./components/RegisterCrateForm";
 
 const App = () => {
   const [account, setAccount] = useState(null);
 
-  const handleConnect = (addr, web3Instance) => {
+  const handleConnect = (addr) => {
     setAccount(addr);
-    // You can now use web3Instance to interact with the contract
   };
 
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>💊 Medicine Supply Chain DApp</h1>
       <MetaMaskAuth onConnect={handleConnect} />
+      {account && <RegisterCrateForm manufacturerAddress={account} />}
     </div>
   );
 };
